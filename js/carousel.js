063a362c28a63f60b5dac54710ed52c845cf4369
Ilaniya <evgeniya.pavlovich@gmail.com>
2021-02-20 15:12:40 +0200

    add js carousel

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+class Carousel {
+  constructor(images) {
+    this.images = images;
+    this.currIndex = 0;
+    this.showCurrentImage();
+    document
+      .querySelector(".prev")
+      .addEventListener("click", () => this.prevImage());
+    document
+      .querySelector(".next")
+      .addEventListener("click", () => this.nextImage());
+    setInterval(() => this.nextImage(), 3000);
+  }
+  showCurrentImage() {
+    document.querySelector(".currentImg").src = this.images[this.currIndex];
+  }
+  nextImage() {
+    if (++this.currIndex >= this.images.length) this.currIndex = 0;
+    this.showCurrentImage();
+  }
+  prevImage() {
+    if (--this.currIndex < 0) this.currIndex = this.images.length - 1;
+    this.showCurrentImage();
+  }
+}
+
+new Carousel([
+  "../img/carousel.jpg",
+  "../img/belly-dance-cover.jpg",
+  "../img/altair_trainer-solo.jpg",
+]);
---
+++ js/index.js
@@ -0,0 +1,9 @@
+//дожидаемся полной загрузки страницы
+window.onload = function () {
+  document.addEventListener("click", showBurgerMenu);
+
+  function showBurgerMenu() {
+    document.getElementById("header-burger").classList.toggle("active");
+    document.getElementById("headerMenu").classList.toggle("active");
+  }
+};
